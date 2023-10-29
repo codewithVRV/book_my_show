1253d80fd338ebe55bafb362c14bec353e130105
Vishnu Kumar <vermaji1891@gmail.com>
2023-10-29 12:28:47 +0530

    added movieCard to home page

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,11 +1,22 @@
+import MovieCardImage from '../../Assets/movieCardImage.png'
 import HomeCarosel from "../../Components/HomeCarosel/HomeCarosel";
-import Navbar from "../../Components/Navbar/Navbar";
+import HomeMovieCard from "../../Components/HomeMovieCard/HomeMovieCard";
+import Navbar from "../../Components/Navbar/Navbar"
 
 function Home () {
     return (
         <>
             <Navbar />
             <HomeCarosel />
+            <div className="mt-8 flex justify-center items-center gap-10">
+                
+                <HomeMovieCard  movieImage={MovieCardImage}/>
+                <HomeMovieCard  movieImage={MovieCardImage}/>
+                <HomeMovieCard  movieImage={MovieCardImage}/>
+                <HomeMovieCard  movieImage={MovieCardImage}/>
+                <HomeMovieCard  movieImage={MovieCardImage}/>
+
+            </div>
         </>
     )
 }
