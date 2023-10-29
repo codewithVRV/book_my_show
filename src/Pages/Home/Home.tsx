25119719c5d1d81a99011364c6417cafa9174bbe
Vishnu Kumar <vermaji1891@gmail.com>
2023-10-29 12:45:14 +0530

    added banner and footer

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,22 +1,34 @@
+import IccBannerImage from '../../Assets/banner.avif'
 import MovieCardImage from '../../Assets/movieCardImage.png'
+import HomeBanner from '../../Components/HomeBanner/HomeBanner';
 import HomeCarosel from "../../Components/HomeCarosel/HomeCarosel";
+import HomeFooter from '../../Components/HomeFooter/HomeFooter';
 import HomeMovieCard from "../../Components/HomeMovieCard/HomeMovieCard";
 import Navbar from "../../Components/Navbar/Navbar"
 
 function Home () {
     return (
+        
         <>
+
             <Navbar />
             <HomeCarosel />
-            <div className="mt-8 flex justify-center items-center gap-10">
-                
-                <HomeMovieCard  movieImage={MovieCardImage}/>
-                <HomeMovieCard  movieImage={MovieCardImage}/>
-                <HomeMovieCard  movieImage={MovieCardImage}/>
-                <HomeMovieCard  movieImage={MovieCardImage}/>
-                <HomeMovieCard  movieImage={MovieCardImage}/>
+            <div className="min-h-[66rem] w-[80vw] mx-auto flex flex-col">
+                <HomeBanner image={IccBannerImage} />
+                <div className='text-2xl font-semibold text-black mt-4'>
+                    Recommended Movies
+                </div>
+                <div className="mt-8 flex justify-center items-center gap-4">
+                    <HomeMovieCard movieImage={MovieCardImage} />
+                    <HomeMovieCard movieImage={MovieCardImage} />
+                    <HomeMovieCard movieImage={MovieCardImage} />
+                    <HomeMovieCard movieImage={MovieCardImage} />
+                    <HomeMovieCard movieImage={MovieCardImage} />
+           
+                </div>
 
             </div>
+            <HomeFooter />
         </>
     )
 }
