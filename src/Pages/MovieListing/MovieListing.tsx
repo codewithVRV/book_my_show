d52c700f967f1ac9e041606d71f1c35e1f7c6aae
Vishnu Kumar <vermaji1891@gmail.com>
2023-11-04 12:20:21 +0530

    added theatreName card to the movieListing page

--- src/Pages/MovieListing/MovieListing.tsx
+++ src/Pages/MovieListing/MovieListing.tsx
@@ -1,9 +1,11 @@
 import ShowTimingCard from "../../Components/ShowTimingCard/ShowTimingCard"
+import TheatreNameCard from "../../Components/TheatreNameCard/TheatreNameCard"
 
 function MovieListing () {
     return (
         <>
             <ShowTimingCard />
+            <TheatreNameCard />
         </>
     )
 }
