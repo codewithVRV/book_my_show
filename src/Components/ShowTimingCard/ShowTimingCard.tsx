391ca760afda5b3aa28c92713e7f83b4de889e11
Vishnu Kumar <vermaji1891@gmail.com>
2023-11-04 12:57:44 +0530

    added cursor pointer to showcard

--- src/Components/ShowTimingCard/ShowTimingCard.tsx
+++ src/Components/ShowTimingCard/ShowTimingCard.tsx
@@ -1,6 +1,6 @@
 function ShowTimingCard () {
     return (
-        <div className="mx-2 my-2 group relative w-32 py-1 px-2 flex flex-col items-center justify-center text-sm text-green-400 rounded-lg border border-black">
+        <div className="cursor-pointer mx-2 my-2 group relative w-32 py-1 px-2 flex flex-col items-center justify-center text-sm text-green-400 rounded-lg border border-black">
             <div>
                 10:45 PM
             </div>
@@ -9,7 +9,7 @@ function ShowTimingCard () {
                 Dolby Atmos
             </div>
 
-            <div className="z-10 bg-white absolute top-[100%] left-[10%] text-black hidden group-hover:block shadow-lg py-2 px-4">
+            <div className="z-10 bg-white absolute top-[100%] left-[10%]  text-black hidden group-hover:block shadow-lg py-2 px-4">
                 <div className="flex flex-col items-center justify-center">
                     <div>
                         Rs. 200
