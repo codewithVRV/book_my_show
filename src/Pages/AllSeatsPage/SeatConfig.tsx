f3cf38aff2575018004d758804f7e67d1a171a65
Vishnu Kumar <vermaji1891@gmail.com>
2023-11-16 11:37:16 +0530

    added types for seat config and used in a state

--- src/Pages/AllSeatsPage/SeatConfig.tsx
+++ src/Pages/AllSeatsPage/SeatConfig.tsx
@@ -1,22 +1,35 @@
 // import { useLocation } from "react-router-dom";
 
-import {useEffect} from 'react'
+import {useEffect, useState} from 'react'
 import { useLocation } from "react-router-dom";
 
 import HomeLayout from "../../Layouts/HomeLayout";
 import processSeatConfig from '../../Utils/ProcessSeatConfig';
 
+
+type Seat = {
+    number: number,
+    status: number,
+}
+
+type Row = {
+    number: string,
+    seats: [Seat]
+}
+
+type SeatState = [Row]
+
 function SeatConfig () {
     const {state} = useLocation()
+    const [seats, setSeats] = useState<SeatState>([{number: "", seats : [{number: 0, status: 0}]}])
 
-    console.log("state from seatConfig",state)
     useEffect(() => {
-        console.log(processSeatConfig(state.config))
+        const seatConfigJson = processSeatConfig(state.config);
+        setSeats(seatConfigJson.rows)
     }, [])
     return (
         <HomeLayout>
-            <h1>AllSeatsPage Here</h1>
-            {/* <h1>{state}</h1> */}
+            <h1>AllSeatsPage Here{seats.length}</h1>
         </HomeLayout>
     )
 }
