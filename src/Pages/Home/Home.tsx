31c0ff76682baee4d5b7ed776f69193b6a7ad798
Vishnu Kumar <vermaji1891@gmail.com>
2024-01-19 12:32:48 +0530

    fix movie craousel

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -84,8 +84,8 @@ function Home () {
             
                     </div>
                     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                    <a href="#slide4" className="btn btn-circle">❮</a> 
-                    <a href="#slide2" className="btn btn-circle">❯</a>
+                    {/* <a href="#slide4" className="btn btn-circle">❮</a>  */}
+                    {/* <a href="#slide2" className="btn btn-circle">❯</a> */}
                     </div>
                 </div> 
             
